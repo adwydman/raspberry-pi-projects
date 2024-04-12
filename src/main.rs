67e6d1f624e7029943977b402cde0a4b97a162a4
Adrian Wydmanski <awydmanski@cribl.io>
2024-04-11 22:46:45 -0500

    update led number

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,8 @@ use rppal::gpio::Gpio;
 use rppal::system::DeviceInfo;
 
 // Gpio uses BCM pin numbering. BCM GPIO 23 is tied to physical pin 16.
-const GPIO_LED1: u8 = 27;
-const GPIO_LED2: u8 = 23;
+const GPIO_LED1: u8 = 23;
+// const GPIO_LED2: u8 = 23;
 
 fn main() -> Result<(), Box<dyn Error>> {
     println!("Blinking an LED on a {}.", DeviceInfo::new()?.model());
